3513a9f1a5872d1a0c3d686fb62944a89fcdaadd
Ubiracy Santos <ubiracy.santos@hotmail.com>
2017-06-08 16:17:44 -0300

    .

--- CineSky/app/src/main/java/br/com/cinesky/VideoActivity.java
+++ CineSky/app/src/main/java/br/com/cinesky/VideoActivity.java
@@ -4,17 +4,22 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import br.com.cinesky.controls.FullscreenVideoLayout;
 import br.com.cinesky.controls.FullscreenVideoView;
 
 public class VideoActivity extends Activity {
 
-    FullscreenVideoView videoView;
+    FullscreenVideoLayout videoView;
     Intent mIntent;
 
     public VideoActivity(){
@@ -26,20 +31,45 @@ public class VideoActivity extends Activity {
 
         super.onCreate(savedInstanceState);
 
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.video_stream);
 
         mIntent = getIntent();
 
-        videoView = (FullscreenVideoView)findViewById(R.id.fslFilme);
+        FrameLayout frmFilmeAcao = (FrameLayout) findViewById(R.id.frmFilmeAcao);
+        frmFilmeAcao.bringToFront();
+
+        ImageView btnVoltar = (ImageView) findViewById(R.id.btnVoltar);
+        btnVoltar.bringToFront();
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VideoActivity.this.finish();
+            }
+        });
+
+        videoView = (FullscreenVideoLayout) findViewById(R.id.fslFilme);
         videoView.setActivity(this);
+        videoView.setFullscreen(false);
+        videoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
 
         try {
-            Uri videoUri = Uri.parse("https://openload.co/stream/L2jIQsJ7xBE~1495675175~131.0.0.0~63U111Xw?mime=true");
+            Uri videoUri = Uri.parse("https://www.quirksmode.org/html5/videos/big_buck_bunny.mp4");
             videoView.setVideoURI(videoUri);
+            videoView.setShouldAutoplay(true);
+            videoView.performClick();
+            videoView.findViewById(R.id.vcv_img_fullscreen).setVisibility(View.INVISIBLE);
+            videoView.findViewById(R.id.vcv_seekbar).setBackgroundColor(Color.TRANSPARENT);
+            videoView.findViewById(R.id.rel_videocontrols).setBackgroundColor(Color.TRANSPARENT);
+
             String nomeFilme = mIntent.getStringExtra("nomeFilme");
             TextView tvNomeFilme = (TextView)findViewById(R.id.tvNomeFilme);
             tvNomeFilme.setText(nomeFilme);
